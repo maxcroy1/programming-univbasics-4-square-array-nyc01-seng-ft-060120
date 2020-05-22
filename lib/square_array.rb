96483fdfe7c3cdc7b7d293bed941002fe0864055
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 15:07:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
   array.length.times do |index|
-    newArray << array[index] ** 2 
+    new_array << array[index] ** 2 
   end
-  newArray
+  new_array
 end
